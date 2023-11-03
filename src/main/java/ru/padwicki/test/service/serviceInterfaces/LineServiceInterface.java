e9006427d4525c8b3d85e765448c2c0ffee65040
agnuscrucifixion <nikitabysko123@gmail.com>
2023-11-03 17:13:11 +0300

    redundant annotation

--- src/main/java/ru/padwicki/test/service/serviceInterfaces/LineServiceInterface.java
+++ src/main/java/ru/padwicki/test/service/serviceInterfaces/LineServiceInterface.java
@@ -1,8 +1,7 @@
 package ru.padwicki.test.service.serviceInterfaces;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface LineServiceInterface {
-    String findCountOfLetters(@RequestParam @NotNull String line);
+    String findCountOfLetters(@RequestParam String line);
 }
